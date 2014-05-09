ec0843f530caa2f946815021f2914d244bd2e999
Vincent <liataian@hotmail.com>
2014-05-09 20:29:11 +0800

    修改一些錯誤

--- hw4-course/js/checker.js
+++ hw4-course/js/checker.js
@@ -6,7 +6,7 @@ var startButton = $('.hw4-start-button'), // 「開始掃描」按鈕
 // 垃圾社團列表
 var junkGroups = [];
 
-$getJSON('http://jsbin.com/jaziroja/1',{},function(data){
+$.getJSON('http://jsbin.com/jaziroja/1',{},function(data){
      // 將每筆資料的 GID 放進 junkGroups 陣列中。
     //
     // ...
@@ -19,19 +19,19 @@ $getJSON('http://jsbin.com/jaziroja/1',{},function(data){
 
 // 用 Ajax 自 http://spamgroup.tonyq.org/groups/jsonp 取得垃圾社團列表
 
-$.ajax('http://spamgroup.tonyq.org/groups/jsonp', {
-  dataType: 'jsonp',
-  jsonp: 'jsonp',
-  success: function(data){
+//$.ajax('http://spamgroup.tonyq.org/groups/jsonp', {
+  //dataType: 'jsonp',
+  //jsonp: 'jsonp',
+  //success: function(data){
     // 將每筆資料的 GID 放進 junkGroups 陣列中。
     //
     // ...
     //   junkGroups.push(data[i].GID);
     // ...
     //
-    startButton.removeAttr('disabled').removeClass('disabled');
-  }
-});
+    //startButton.removeAttr('disabled').removeClass('disabled');
+  //}
+//});
 
 // 設定 Facebook AppID
 window.fbAsyncInit = function(){
