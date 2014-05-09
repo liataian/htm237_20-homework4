942200db14724df17f222b61f613bfad0e84c2c6
Vincent <liataian@hotmail.com>
2014-05-09 20:45:54 +0800

    改了一堆

--- hw4-course/js/checker.js
+++ hw4-course/js/checker.js
@@ -8,8 +8,6 @@ var junkGroups = [];
 
 $.getJSON('http://jsbin.com/jaziroja/1',{},function(data){
      // 將每筆資料的 GID 放進 junkGroups 陣列中。
-    //
-    // ...
 	var i;
 	for(i=0;i<data.length;i+=1){
 	 junkGroups.push(data[i].GID);
@@ -36,7 +34,7 @@ $.getJSON('http://jsbin.com/jaziroja/1',{},function(data){
 // 設定 Facebook AppID
 window.fbAsyncInit = function(){
   FB.init({
-    appId: '259195264246285', // 若可以，請換成自己的 App ID !
+    appId: '1449150861994024', // 若可以，請換成自己的 App ID !
     status: true
   });
 
@@ -47,9 +45,23 @@ window.fbAsyncInit = function(){
     $('.hw4-complete').remove(); // 移除「掃描完成」
 
     // 1. 讓使用者登入此 Facebook App (FB.login)
-    // 2. 以 FB.api 拿到使用者的 group 列表
-    // 拿到使用者 group 列表的 response 之後：
-    // results.after('<div class="hw4-complete alert alert-info">掃描完成</div>');
+	FB.login(function(){
+	console.log('登入啦啦啦!');
+	// 2. 以 FB.api 拿到使用者的 group 列表
+	FB.api('/me/groups',function(resp){
+	console.log('USER GROUPS:',resp.data);
+	var i;
+	for(i=0;i<resp.data.length;i+=1){
+	 group = resp.data[i];
+	 if(junkGroups.indexOf(group.id)!==-1){
+	    results.append('<tr><td>'+group.id+'</td><td>'+group.name+'</td></tr>')
+	 }
+	}
+	
+	// 拿到使用者 group 列表的 response 之後：
+	results.after('<div class="hw4-complete alert alert-info">掃描完成</div>');
+	});	
+	},{scope:'user_groups'});
 
   });
 };
